a476d726620bb3ccb26801b048444b408e662434
Elisei <brunoelisei@o2ti.com>
2023-07-21 12:39:20 -0300

    PagBank 😍 Magento

--- Gateway/Request/BillingAddressDataRequest.php
+++ Gateway/Request/BillingAddressDataRequest.php
@@ -78,10 +78,13 @@ class BillingAddressDataRequest implements BuilderInterface
 
         $billingAddress = $orderAdapter->getBillingAddress();
         if ($billingAddress) {
+            $postalCode = preg_replace('/[^0-9]/', '', (string) $billingAddress->getPostcode());
+            $postalCode = str_pad($postalCode, 8, '0', STR_PAD_RIGHT);
+
             $result[self::CUSTOMER] = [
                 self::ADDRESS           => [
                     // phpcs:ignore Generic.Files.LineLength
-                    AddressDataRequest::POSTAL_CODE   => preg_replace('/[^0-9]/', '', (string) $billingAddress->getPostcode()),
+                    AddressDataRequest::POSTAL_CODE   => $postalCode,
                     // phpcs:ignore Generic.Files.LineLength
                     AddressDataRequest::STREET        => $this->config->getValueForAddress($billingAddress, AddressDataRequest::STREET),
                     // phpcs:ignore Generic.Files.LineLength
